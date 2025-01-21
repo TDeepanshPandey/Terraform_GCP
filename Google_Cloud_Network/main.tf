3b43bc30eb9f89bcaa981d2c22488f7151e9397e
Deepansh Pandey <easypandey.dp@gmail.com>
2025-01-21 23:38:09 +0100

    Cloud Network Finished

--- Google_Cloud_Network/main.tf
+++ Google_Cloud_Network/main.tf
@@ -12,9 +12,19 @@ resource "google_compute_subnetwork" "sub-sg" {
     name = "sub-sg"
     network = google_compute_network.custom-vpc-tf.id
     ip_cidr_range = "10.1.0.0/24"
-    region = "asia-southeast1"
+    region = "europe-west3"
     private_ip_google_access = true
 } 
+
+resource "google_compute_firewall" "allow-icmp" {
+    name = "allow-icmp"
+    network = google_compute_network.custom-vpc-tf.id
+    allow {
+        protocol = "icmp"
+    }
+    source_ranges = ["*"]
+    priority = 455
+}
 
 output "auto" {
     value = google_compute_network.auto-vpc-tf.id
